38b7608bf45589f3a6d9f4f31e9e931e35ea263b
@TIP209 <>
2022-09-21 11:30:16 +0900

    완성

--- 06/character_grass.py
+++ 06/character_grass.py
@@ -1,16 +1,50 @@
 from pico2d import *
+import math
 
 open_canvas()
 
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-x = 0
-while(x<800):
+def draw_char_and_grass(x, y):
     clear_canvas_now()
     grass.draw_now(400, 30)
-    character.draw_now(x, 90)
-    x = x+2
-    delay(0.01)
+    character.draw_now(x, y)
+
+
+x1 = 400
+y1 = 90
+R=180
+while(True):   
+    while(R<360):
+        draw_char_and_grass(400 + 210*math.sin(R/360*2*math.pi), 300 + 210*math.cos(R/360*2*math.pi))
+        R = R+1
+        delay(0.01)
+    R=0
+    while(R<180):
+        draw_char_and_grass(400 + 210*math.sin(R/360*2*math.pi), 300 + 210*math.cos(R/360*2*math.pi))
+        R = R+1
+        delay(0.01)
+    while(x1<800):
+        draw_char_and_grass(x1, y1)
+        x1 = x1+2
+        delay(0.01)
+    while(y1<550):
+        draw_char_and_grass(x1, y1)
+        y1 = y1+2
+        delay(0.01)
+    while(x1>0):
+        draw_char_and_grass(x1, y1)
+        x1 = x1-2
+        delay(0.01)
+    while(y1>90):
+        draw_char_and_grass(x1, y1)
+        y1 = y1-2
+        delay(0.01)
+    while(x1<400):
+        draw_char_and_grass(x1, y1)
+        x1 = x1+2
+        delay(0.01)
+    
 
 close_canvas()
