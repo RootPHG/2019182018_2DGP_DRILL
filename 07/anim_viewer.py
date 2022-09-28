61385fd31473404ff1ca46c85832db114f29344e
RootPHG <phg0517@gmail.com>
2022-09-28 22:16:33 +0900

    오류 수정, 테스트 이미지 삭제

--- 07/anim_viewer.py
+++ 07/anim_viewer.py
@@ -1,15 +1,19 @@
-from pico2d import*
+from pico2d import *
 
 open_canvas()
+
 
 rockman = load_image('img.png')
 
 frame = 0
+y = 0
 for x in range(0, 800, 5):
     clear_canvas()
-    rockman.clip_draw(frame * 130, 0, 130, 360, x, 90)
+    rockman.clip_draw(frame * 170, y * 170, 170, 180, x, 90)
+    update_canvas()
     frame = (frame + 1) % 5
-    delay(0.01)
+    y = (y+1) % 2
+    delay( 0.05)
     get_events()
 
 
