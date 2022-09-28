f3d0bc7a0cdd9060ee71c8bf3b81a6dd61880fb0
RootPHG <phg0517@gmail.com>
2022-09-28 23:54:21 +0900

    마무리 (함수 생성 실패)

--- 07/anim_viewer.py
+++ 07/anim_viewer.py
@@ -2,19 +2,48 @@ from pico2d import *
 
 open_canvas()
 
-
-rockman = load_image('img.png')
+girl = load_image('running.png')
 
 frame = 0
-y = 0
-for x in range(0, 800, 5):
+
+direction = 1
+y = 90
+for x in range(0, 800, 10):
     clear_canvas()
-    rockman.clip_draw(frame * 170, y * 170, 170, 180, x, 90)
+    girl.clip_draw(frame * 60, direction * 60, 60, 60, x, y)
     update_canvas()
-    frame = (frame + 1) % 5
-    y = (y+1) % 2
+    frame = (frame + 1) % 8
     delay( 0.05)
     get_events()
 
+direction = 0
+
+for y in range(90, 580, 10):
+    clear_canvas()
+    girl.clip_draw(frame * 60, direction * 60, 60, 60, x, y)
+    update_canvas()
+    frame = (frame + 1) % 8
+    delay( 0.05)
+    get_events()
+
+direction = 2
+
+for x in range(800, 0, -10):
+    clear_canvas()
+    girl.clip_draw(frame * 60, direction * 60, 60, 60, x, y)
+    update_canvas()
+    frame = (frame + 1) % 8
+    delay( 0.05)
+    get_events()
+
+direction = 3
+
+for y in range(580, 90, -10):
+    clear_canvas()
+    girl.clip_draw(frame * 60, direction * 60, 60, 60, x, y)
+    update_canvas()
+    frame = (frame + 1) % 8
+    delay( 0.05)
+    get_events()
 
 close_canvas()
