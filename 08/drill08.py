a1fc99ae06f2efd8726b298d2ee14621106fab4a
RootPHG <phg0517@gmail.com>
2022-10-01 11:35:53 +0900

    Create drill08.py

---
+++ 08/drill08.py
@@ -0,0 +1,18 @@
+from pico2d import *
+import turtle
+
+open_canvas()
+
+running_boy = load_image('animation_sheet.png')
+
+frame = 0
+
+direction = 3
+
+
+x, y = 0
+
+def drawing(direction):
+    claer_canvas()
+    running_boy.clip_draw(frame* 
+
