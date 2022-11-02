23340bdf79bf39e1c47a7eabc99c7c53de8e04e0
Park Seongro <mypuhaba@gmail.com>
2022-11-02 10:23:26 +0900

    Update boy.py

--- Lecture11_Character_Controller/boy.py
+++ Lecture11_Character_Controller/boy.py
@@ -2,7 +2,7 @@ from pico2d import *
 
 # 이벤트 정의
 # RD, LD, RU, LU= 0, 1, 2, 3
-RD, LD, RU, LU = range(4)
+RD, LD, RU, LU, TIMER = range(5)
 
 key_event_table = {
     (SDL_KEYDOWN, SDLK_RIGHT) : RD,
@@ -13,41 +13,95 @@ key_event_table = {
 
 
 class IDLE:
-    def enter():    # 상태에 들어갈 때 행하는 액션
+    def enter(self, event):    # 상태에 들어갈 때 행하는 액션
         print('ENTER IDLE')
+        self.dir = 0
+        self.timer = 1000
         pass
-    def exit():     # 상태를 나올 때 행하는 액션
+    def exit(self):     # 상태를 나올 때 행하는 액션
         print('EXIT IDLE')
         pass
-    def do():       # 상태에 있을 때 지속적으로 행하는 행위
+    def do(self):       # 상태에 있을 때 지속적으로 행하는 행위
+        self.frame = (self.frame + 1) % 8
+        self.timer -= 1
+        if self.timer == 0:
+            self.add_event(TIMER)
         pass
-    def draw():
+    def draw(self):
+        if self.face_dir == 1:
+            self.image.clip_draw(self.frame * 100, 300, 100, 100, self.x, self.y)
+        else:
+            self.image.clip_draw(self.frame * 100, 200, 100, 100, self.x, self.y)
+
         pass
 
 class RUN:
     @staticmethod
-    def enter():
+    def enter(self, event):
         print('ENTER RUN')
+        # 방향을 결정해야 하는데, 뭘 근거로? 어떤 키가 눌렸기때문에?
+        # 키 이벤트 정보가 필요.
+        if event == RD:
+            self.dir += 1
+        elif event == LD:
+            self.dir -= 1
+        elif event == RU:
+            self.dir -= 1
+        elif event == LU:
+            self.dir += 1
         pass
 
     @staticmethod
-    def exit():
+    def exit(self):
         print('EXIT RUN')
+        self.face_dir = self.dir
         pass
 
     @staticmethod
-    def do():
+    def do(self):
+        self.frame = (self.frame + 1) % 8
+        # x 좌표 변경, 달리기
+        self.x += self.dir
+        self.x = clamp(0, self.x, 800)
         pass
 
     @staticmethod
-    def draw():
+    def draw(self):
+        if self.dir == -1:
+            self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
+        elif self.dir == 1:
+            self.image.clip_draw(self.frame*100, 100, 100, 100, self.x, self.y)
+
         pass
 
+
+
+class SLEEP:
+    def enter(self, event):    # 상태에 들어갈 때 행하는 액션
+        print('ENTER IDLE')
+        pass
+
+    def exit(self):     # 상태를 나올 때 행하는 액션
+        print('EXIT IDLE')
+        pass
+
+    def do(self):       # 상태에 있을 때 지속적으로 행하는 행위
+        self.frame = (self.frame + 1) % 8
+        pass
+
+    def draw(self):
+        if self.face_dir == -1:
+            self.image.clip_composite_draw(self.frame * 100, 200, 100, 100, -3.141592/2, '', self.x, self.y-25, 100, 100)
+        else:
+            self.image.clip_composite_draw(self.frame * 100, 300, 100, 100, 3.141592/2, '', self.x, self.y-25, 100, 100)
+
+        pass
 
 #3 상태 변환 구현
 next_state = {
-    IDLE :  {RU: RUN,   LU: RUN,    RD: RUN,    LD: RUN},
-    RUN :   {RU: IDLE,  LU: IDLE,   RD: IDLE,   LD: IDLE}
+    SLEEP:  {RD: RUN,   LD: RUN,    RU: RUN,    LU: RUN, TIMER: SLEEP},
+    IDLE :  {RU: RUN,   LU: RUN,    RD: RUN,    LD: RUN, TIMER: SLEEP},
+    RUN :   {RU: IDLE,  LU: IDLE,   RD: IDLE,   LD: IDLE,}
 }
 
 
@@ -56,10 +110,13 @@ next_state = {
 
 class Boy:
 
+    def add_event(self, event):
+        self.q.insert(0, event)
+
     def handle_events(self, event):
         if (event.type, event.key) in key_event_table:
             key_event = key_event_table[(event.type, event.key)]
-            self.q.insert(0, key_event)
+            self.add_event(key_event)
 
 
 
@@ -86,17 +143,17 @@ class Boy:
 
         self.q = []
         self.cur_state = IDLE
-        self.cur_state.enter()  # 초기 상태의 enter 실행
+        self.cur_state.enter(self, None)  # 초기 상태의 enter 실행
 
     def update(self):
-        self.cur_state.do() # 현재 상태의 do 실행
+        self.cur_state.do(self) # 현재 상태의 do 실행
 
         # 이벤트를 확인해서, 이벤트가 있으면 이벤트 변환 처리
         if self.q: # 큐 에 이벤트가 있으면
             event = self.q.pop()
-            self.cur_state.exit()   # 현재 상태를 나가고
+            self.cur_state.exit(self)   # 현재 상태를 나가고
             self.cur_state = next_state[self.cur_state][event]    # 다음 상태로 이동
-            self.cur_state.enter()
+            self.cur_state.enter(self, event)
 
 
         # self.frame = (self.frame + 1) % 8
@@ -106,14 +163,5 @@ class Boy:
 
 
     def draw(self):
-        self.cur_state.draw()
+        self.cur_state.draw(self)
 
-        # if self.dir == -1:
-        #     self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
-        # elif self.dir == 1:
-        #     self.image.clip_draw(self.frame*100, 100, 100, 100, self.x, self.y)
-        # else:
-        #     if self.face_dir == 1:
-        #         self.image.clip_draw(self.frame * 100, 300, 100, 100, self.x, self.y)
-        #     else:
-        #         self.image.clip_draw(self.frame * 100, 200, 100, 100, self.x, self.y)
